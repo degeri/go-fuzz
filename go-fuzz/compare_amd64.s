1f7f8b33b13185121792a5d3adbd048c1707d077
Josh Bleecher Snyder <josharian@gmail.com>
2019-02-09 17:49:38 -0800

    speed up cover body comparison with SSE2

--- go-fuzz/compare_amd64.s
+++ go-fuzz/compare_amd64.s
@@ -1,41 +1,43 @@
 #include "textflag.h"
 
+// compareCoverBody1 compares every corresponding byte of base and cur, and
+// reports whether cur has any entries bigger than base.
 // func compareCoverBody1(base, cur *byte) bool
 TEXT ·compareCoverBody1(SB), NOSPLIT, $0-17
 	MOVQ	base+0(FP), SI
 	MOVQ	cur+8(FP), DI
-	MOVQ	$65535, AX	// loop counter (CoverSize)
-	MOVQ	$0, R10		// ret
-	BYTE	$0x90		// nop
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
-	BYTE	$0x90
+	XORQ	CX, CX	// loop counter
+	XORQ	R10, R10	// ret
+
+	// Fill X0 with 128.
+	MOVL	$128, AX
+	MOVD	AX, X0
+	PUNPCKLBW X0, X0
+	PUNPCKLBW X0, X0
+	PSHUFL $0, X0, X0
+
+	// TODO: nops for loop target alignment?
 loop:
-	MOVBQZX 0(DI)(AX*1), R9
-	TESTB	R9, R9
-	JNZ	non_zero
-continue:
-	SUBQ	$1, AX
-	JNS	loop
-	JMP	done
-non_zero:
-	MOVBQZX 0(SI)(AX*1), R8
-	CMPB	R8, R9
-	JB	new_cover
-	JMP	continue
-new_cover:
-	MOVB	$1, R10
-done:
+	MOVOU	(SI)(CX*1), X1
+	MOVOU	(DI)(CX*1), X2
+	// Add -128 to each byte.
+	// This lets us use signed comparison below to implement unsigned comparison.
+	PSUBB	X0, X1
+	PSUBB	X0, X2
+	// Compare each byte
+	PCMPGTB	X1, X2 // X2 > X1
+	// Extract top bit of each elem.
+	PMOVMSKB X2, AX
+	// If any bits were set, then some elem of X2 (cur) was bigger than some elem of X1 (base).
+	TESTQ	AX, AX
+	JNZ	yes
+	LEAQ	16(CX), CX	// CX += 16
+	BTL	$16, CX	// have we reached 65536 (CoverSize)?
+	JCS	ret
+	JMP	loop
+yes:
+	MOVQ	$1, R10
+ret:
 	MOVB	R10, ret+16(FP)
 	RET
-
-
 
